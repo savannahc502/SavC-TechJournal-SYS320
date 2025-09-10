fb5937270bc08c4ba7d981a6be4adf04aa5d972c
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-09-10 01:08:53 -0400

    Update save_list_ps1_files.ps1

--- week02/save_list_ps1_files.ps1
+++ week02/save_list_ps1_files.ps1
@@ -1,1 +1,9 @@
+# List # List all the files in your working directory that has the extension ".ps1" 
+# and save the results to out.csv file in "outfolder" directory
+$files = Get-ChildItem
 
+$folderPath = "$PSScriptRoot/outfolder/"
+$filePath = Join-Path $folderPath "out.csv"
+
+$files | Where-Object { $_.Extension -eq ".ps1" } |
+Export-Csv -Path $filePath
