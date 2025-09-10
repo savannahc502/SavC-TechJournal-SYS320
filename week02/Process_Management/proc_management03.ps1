cf6de49a585786434f3e88bac68944fc7395a779
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-09-10 02:44:57 -0400

    Update proc_management03.ps1

--- week02/Process_Management/proc_management03.ps1
+++ week02/Process_Management/proc_management03.ps1
@@ -1,4 +1,9 @@
 # Lists every stopped service, orders it alphabetically, and saves it to a csv file
-$StoppedServices = Get-Service | Where-Object { $_.Status -eq 'Stopped' } | Sort-Object DisplayName 
 
-$StoppedServices | Export-Csv -Path "$PSScriptRoot\stopped_services.csv"
+# Original Script, Correct but too messy
+# $StoppedServices = Get-Service | Where-Object { $_.Status -eq 'Stopped' } | Sort-Object DisplayName 
+# $StoppedServices | Export-Csv -Path "$PSScriptRoot\stopped_services.csv"
+
+# Get all stopped services, sort by DisplayName, and export selected properties to CSV
+$StoppedServices = Get-Service | Where-Object { $_.Status -eq 'Stopped' } | Sort-Object DisplayName
+$StoppedServices | Export-Csv -Path "$PSScriptRoot\stopped_services.csv" -NoTypeInformation
