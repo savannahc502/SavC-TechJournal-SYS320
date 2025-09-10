5612d0480197ee510ca6f9b806161329d6dfdebe
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-09-10 01:30:52 -0400

    Update csv_to_log_recursive.ps1

--- week02/csv_to_log_recursive.ps1
+++ week02/csv_to_log_recursive.ps1
@@ -2,6 +2,11 @@
 # getting into the child directory), find every .csv file and change their extensions
 # to .log. Then, recursively display all the files 
 
+# Old Code, Did Not Work:
+# $files = Get-ChildItem -File -Recurse
+# $files | ForEach-Object -Process { $_.Name -replace '.csv', '.log' }
+# Get-ChildItem -Recurse -File
+
 $files = Get-ChildItem -File -Recurse
-$files | ForEach-Object -Process { $_.Name -replace '.csv', '.log' }
-Get-ChildItem -Recurse -File
+$files | ForEach-Object -Process { Rename-Item $_.FullName ($_.Name -replace '.csv', '.log') }
+Get-ChildItem -File -Recurse
