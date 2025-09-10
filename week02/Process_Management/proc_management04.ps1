5524b3f5afd08502c599b3edcb53deaa69d99673
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-09-10 03:04:33 -0400

    Update proc_management04.ps1

--- week02/Process_Management/proc_management04.ps1
+++ week02/Process_Management/proc_management04.ps1
@@ -1,6 +1,15 @@
 # If an instance of it is not running already,  starts Google Chrome web browser and directs it Champlain.edu
 # If an instance is already running, stops it
 
+# Old Code. Worked but nothing printed for the screenshot. 
+# $chrome = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
+# if (-not $chrome) {
+#    Start-Process "chrome.exe" "https://www/champlain.edu"
+# }
+#    else {
+#    Stop-Process -Name "chrome" -Force
+# }
+
 $chrome = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
 
 if (-not $chrome) {
