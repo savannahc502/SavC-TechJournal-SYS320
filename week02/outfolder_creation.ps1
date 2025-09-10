044462539bcba0c27389a44215edaa932ab5658b
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-09-10 00:55:27 -0400

    Update outfolder_creation.ps1

--- week02/outfolder_creation.ps1
+++ week02/outfolder_creation.ps1
@@ -1,7 +1,7 @@
 # Create a folder called "outfolder" if it does not already exists 
 # Hardcoded to add the folder "outfolder" 
 
-$folderpath="PSScriptRoot\outfolder_test"
+$folderpath="$PSScriptRoot\outfolder"
 if (Test-Path $folderpath){
     Write-Host "Folder Already Exists" 
 }
