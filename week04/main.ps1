e00d5eac85ba4044f41ccd80b561e3e17144fa7d
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-09-26 01:07:00 -0400

    new script week4

--- week04/main.ps1
+++ week04/main.ps1
@@ -1,7 +1,8 @@
-﻿# Load function from apache-logs.ps1 and allow user input
+﻿# Load function from apache-logs.ps1 and parsing-apache-logs.ps1 and allow user input
 
-# Dot source function
-. .\apache-logs.ps1
+# Dot source functions
+. (Join-Path $PSScriptRoot apache-logs.ps1)
+. (Join-Path $PSScriptRoot parsing-apache-logs.ps1)
 
 # User Input
 $page = Read-Host "Enter the page visited (ex. index.html)"
@@ -10,3 +11,4 @@ $browserName = Read-Host "Enter the name of the browser (ex. Chrome)"
 
 # Call Function
 accessedPages $page $code $browserName
+ApacheLogs1
---
+++ week04/parsing-apache-logs.ps1
@@ -0,0 +1,21 @@
+﻿function ApacheLogs1(){
+$logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotFormatted.Length; $i++) {
+    $words = $logsNotFormatted[$i].Split(" ");
+    $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
+                                        "Time" = $words[3].Trim('['); `
+                                        "Method" = $words[5].Trim('"'); `
+                                        "Page" = $words[6]; `
+                                        "Protocol" = $words[7].Trim('"'); `
+                                        "Response" = $words[8]; `
+                                        "Refrrer" = $words[10]; `
+                                        "Client" = $words[11..($words.Count - 1)] -join ' ' }}
+
+return $tableRecords | Where-Object {$_.IP -ilike "10.*" }
+
+}
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -Autosize -Wrap
