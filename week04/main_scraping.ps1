7a3618b20bab64da673b22eda6058398ab658d5f
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-09-26 12:48:56 -0400

    new script week4

---
+++ week04/main_scraping.ps1
@@ -0,0 +1,34 @@
+﻿. "C:\Users\champuser\SavC-TechJournal-SYS320\week04\table-scraping.ps1"
+
+$rawClasses = gatherClasses
+
+$classes = daysTranslator $rawClasses
+
+# Original Table Call
+# $classes | Format-Table -AutoSize
+
+# List all the classes of intructor furkan paligu
+# $classes | Where-Object {$_.Instructor -eq "Furkan Paligu"}
+
+# List all classes of Joyce310 on Mondays, only display class code and time. Sort by start time. 
+# $classes | Where-Object {($_.Location -eq "JOYC 310") -and ($_.days -eq "Monday")} | `
+    # Sort-Object "Time Start" | `
+    # Select-Object "Time Start", "Time End", "Class Code"
+
+# List all intructors that teach at least 1 course in SYS, SEC, NET, FOR, CSI, DAT
+# Sort by start name 
+$its_instructors = $classes | Where-Object {
+                             ($_."Class Code" -like "SYS*") -or `
+                             ($_."Class Code" -like "SEC*") -or `
+                             ($_."Class Code" -like "NET*") -or `
+                             ($_."Class Code" -like "FOR*") -or `
+                             ($_."Class Code" -like "CSI*") -or `
+                             ($_."Class Code" -like "DAT*") } `
+                             | Sort-Object "Instructor" -Unique `
+                             | Select-Object -ExpandProperty "Instructor"
+# $its_instructors
+
+
+# Group all instructors by the number of classes they are teaching
+$classes | Where-Object {$_.Instructor -in $its_instructors } `
+         | Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
---
+++ week04/table-scraping.ps1
@@ -0,0 +1,60 @@
+﻿function gatherClasses() {
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.19/Courses.html
+
+#Get all the tr elements of a HTML document
+$trs = $page.ParsedHTML.body.getElementsByTagName("tr")
+
+#Empty array to hold results
+$FullTable = @()
+for($i = 1; $i -lt $trs.length; $i++){ #going over every tr element
+
+   # get every td element of current tr element
+   $tds = $trs[$i].getElementsByTagName("td")
+
+   # want to separate start time and end time from one time field
+   $Times = $tds[5].innerText.Split("-")
+
+   $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; `
+                                   "Title" = $tds[1].innerText; `
+                                   "Days" = $tds[4].innerText; `
+                                   "Time Start" = $Times[0]; `
+                                   "Instructor" = $tds[6].innerText; `
+                                   "Location" = $tds[9].innerText; `
+                                   }
+}
+
+return $FullTable
+}
+
+
+function daysTranslator($FullTable){
+
+# Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+    # Empty array to hold days for every record
+    $Days = @()
+
+    # If you see "M" -> Monday
+    if($FullTable[$i].Days -ilike "*M*"){  $Days += "Monday"  }
+
+    # If you see "T" followed by T, W, or F -> Tuesday
+    if($FullTable[$i].Days -ilike "*T[W,F]*"){  $Days += "Tuesday"  }
+
+    # If you see a "W" -> Wednesday
+    if($FullTable[$i].Days -ilike "*W*"){  $Days += "Wednesday"  }
+
+    # If you see a "Th" -> Thursday
+    if($FullTable[$i].Days -ilike "*TH*"){  $Days += "Thursday"  }
+
+    # If you see a "F" -> Friday
+    if($FullTable[$i].Days -ilike "*F*"){  $Days += "Friday"  }
+
+    # Make the Switch
+    $FullTable[$i].Days = $Days
+
+}
+return $FullTable
+}
+
