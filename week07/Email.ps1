9cd156556a1671b184f2763e10017172745becc4
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-10-11 13:13:11 -0400

    week 7 edit

---
+++ week07/Email.ps1
@@ -0,0 +1,15 @@
+﻿function SendAlertEmail ($Body){
+
+$From = "savannah.ciak@mymail.champlain.edu"
+$To = "savannah.ciak@mymail.champlain.edu"
+$Subject = "Suspicious Activity" 
+
+$Password = Get-Content "C:\Users\champuser\Documents\app.txt" | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential - ArgumentList $From, $Password
+
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" `
+-port 587 -UseSsl -Credential $Credential
+
+}
+
+SendAlertEmail "Body of Email" 
