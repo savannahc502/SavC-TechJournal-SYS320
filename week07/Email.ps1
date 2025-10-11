a5e3cce0cd2193535df05be286ca647bdb1709a0
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-10-11 13:14:33 -0400

    week 7 start

--- week07/Email.ps1
+++ week07/Email.ps1
@@ -5,7 +5,7 @@ $To = "savannah.ciak@mymail.champlain.edu"
 $Subject = "Suspicious Activity" 
 
 $Password = Get-Content "C:\Users\champuser\Documents\app.txt" | ConvertTo-SecureString -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential - ArgumentList $From, $Password
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" `
 -port 587 -UseSsl -Credential $Credential
