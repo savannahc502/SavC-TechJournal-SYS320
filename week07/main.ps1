d14e66f7fb185ebb64d88898ac93cb0032c745b3
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-10-13 03:03:09 -0400

    FINALLY WORKS

--- week07/main.ps1
+++ week07/main.ps1
@@ -5,7 +5,8 @@ param ([switch]$Main)
 . "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Scheduler.ps1"
 
 if ($Main) {
-    . "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Configuration.ps1"
+    . "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Configuration.ps1"}
+    else {Write-Host "The '-Main' switch was not used, re-run with it to see the menu. Automatic exit starting shortly."}
 
 # Obtaining configuration
 $configuration = Get-Content "C:\Users\champuser\SavC-TechJournal-SYS320\week07\configuration.txt" 
