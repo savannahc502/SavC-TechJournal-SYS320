8e23807ed30632b8828e7f169ed365d389133532
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-10-13 01:56:53 -0400

    Update main.ps1

--- week07/main.ps1
+++ week07/main.ps1
@@ -1,10 +1,10 @@
 ﻿. "C:\Users\champuser\SavC-TechJournal-SYS320\week06\Event-Logs.ps1"
 . "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Email.ps1"
 . "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Scheduler.ps1"
-. "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Configuration.ps1"
+. "C:\Users\champuser\SavC-TechJournal-SYS320\week07\Configuration.ps1" -Silent
 
 # Obtaining configuration
-$configuration = Get-Content "C:\Users\champuser\SavC-TechJournal-SYS320\week07\configuration.txt" -Silent
+$configuration = Get-Content "C:\Users\champuser\SavC-TechJournal-SYS320\week07\configuration.txt" 
 
 # Obtaining at risk users
 $Failed = at_risk_users($configuration[0])
