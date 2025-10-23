2361b8576c58f4ac8ab90b8b0f4a95e32dbaece4
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-10-22 20:07:39 -0400

    midterm

---
+++ week08/IoC_Scrape.ps1
@@ -0,0 +1,27 @@
+﻿function Get-IoCWeb () {
+
+    $url = "http://10.0.17.19/IOC.html"
+    $page_content = Invoke-WebRequest -Uri $url -UseBasicParsing
+    $html_format = $page_content.Content -join "`n"
+
+    $xml_format = [xml]("<root>" + $html_format + "</root>")
+    $format_rows = $xml_format.SelectNodes("//tr")
+
+    $IoC_array = @()
+
+    foreach ($row in $format_rows) {
+        $columns = $row.SelectNodes("td")
+        if ($columns.Count -eq 2) {
+            $pattern = $columns[0].InnerText.Trim()
+            $description = $columns[1].InnerText.Trim()
+
+            $IoC_array += [PSCustomObject]@{
+                Pattern = $pattern
+                Explanation = $description
+            }
+        }
+    }
+    return $IoC_array
+}
+
+Get-IoCWeb | Format-Table -AutoSize
---
+++ week08/get_logs01.ps1
@@ -0,0 +1,21 @@
+﻿function Get-ApacheLogs() {
+    $logs_file = Get-Content "C:\Users\champuser\access.log"
+    $records_array = @()
+
+    for($i=0; $i -lt $logs_file.Count; $i++){
+        $format = $logs_file[$i].Split(" ");
+        $records_array += [PSCustomObject]@{
+            "IP" = $format[0]; `
+            "Time" = $format[3].Trim('['); `
+            "Method" = $format[5].Trim('"'); `
+            "Page" = $format[6]; `
+            "Protocol" = $format[7].Trim('"'); `
+            "Response" = $format[8]; `
+            "Referrer" = $format[10]; `
+            "Client" = $format[11..($format.Count -1)] -join ' ';
+        }
+    }
+    return $records_array | Where-Object { $_.IP -ilike "10.*" }
+}
+
+Get-ApacheLogs | Format-Table -AutoSize -Wrap
