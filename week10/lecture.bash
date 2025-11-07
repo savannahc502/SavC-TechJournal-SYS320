9e42ae4f4011b44f726ecc650e23c0229f9273a2
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-11-06 22:42:02 -0500

    Update lecture.bash

--- week10/lecture.bash
+++ week10/lecture.bash
@@ -2,12 +2,12 @@
 
 file="/var/log/apache2/access.log"
 
-function pageCount(){
+function filterPage2Requests(){
     all_logs=$(grep 'GET /page2.html' "$file" | cut -d' ' -f1,7 | tr -s ' ' | while read ip page; do
         echo "$ip ${page#/}"
     done)
 }
 
-pageCount
+filterPage2Requests
 echo "$all_logs"
 
