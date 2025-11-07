c2f079241d9607997ffaef54f7b9d28cd46f8c7b
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-11-06 22:22:00 -0500

    edits week 10

---
+++ week10/lecture.bash
@@ -0,0 +1,11 @@
+#!bin/bash
+
+all_logs=""
+file="/var/log/apache2/access.log"
+
+function pageCount(){
+all_logs=$(cat "$file" | cut -d' ' -f1,7 | tr -d "[" | sort | uniq -c )
+}
+
+pageCount
+echo "$all_logs"
