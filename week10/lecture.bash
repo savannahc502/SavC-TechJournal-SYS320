1e1d7553cf5222ae5b9a45e9516f3770064f2bca
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-11-06 22:25:32 -0500

    Update lecture.bash

--- week10/lecture.bash
+++ week10/lecture.bash
@@ -1,10 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
-all_logs=""
 file="/var/log/apache2/access.log"
 
 function pageCount(){
-all_logs=$(cat "$file" | cut -d' ' -f1,7 | tr -d "[" | sort | uniq -c )
+    all_logs=$(grep "page2.html" "$file" | cut -d' ' -f1,7 | tr -s ' ' | sort | uniq -c)
 }
 
 pageCount
