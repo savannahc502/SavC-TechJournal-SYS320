9f827a487e4dce546b2bfeaa99633589277f2da5
savannahc502 <savannah.ciak@mymail.champlain.edu>
2025-11-14 10:02:56 -0500

    week 12

---
+++ week12/access.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file="/home/champuser/SavC-TechJournal-SYS320/week12/fileaccesslog.txt"
+time=$(date "+%Y-%m-%d %I:%M:%S %p") 
+echo "File was accessed on $timestamp" >> "$file"
+
+function read_log(){
+	cat $file
+}
+
+echo "To: savannah.ciak@mymail.champlain.edu" > email.txt
+echo "Subject: File Access Log" >> email.txt
+
+read_log >> email.txt
+
+cat email.txt | ssmtp savannah.ciak@mymail.champlain.edu 
