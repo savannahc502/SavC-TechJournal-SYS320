f307b36df933967b6120b99b5828fd52bd9df658
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-11-14 10:07:09 -0500

    Update access.bash

--- week12/access.bash
+++ week12/access.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 file="/home/champuser/SavC-TechJournal-SYS320/week12/fileaccesslog.txt"
-time=$(date "+%Y-%m-%d %I:%M:%S %p") 
+time=$(date "+%Y-%m-%d %I-%M-%S %p")
 echo "File was accessed on $time" >> "$file"
 
-function read_log(){
-	cat $file
-}
+{ echo "To: savannah.ciak@mymail.champlain.edu"
+  echo "Subject: File Access Log"
+  cat "$file"
+} > email.txt
 
-echo "To: savannah.ciak@mymail.champlain.edu" > email.txt
-echo "Subject: File Access Log" >> email.txt
-
-read_log >> email.txt
-
-cat email.txt | ssmtp savannah.ciak@mymail.champlain.edu 
+ssmtp savannah.ciak@mymail.champlain.edu < email.txt
