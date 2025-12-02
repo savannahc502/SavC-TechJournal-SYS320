a4db61b9508ccfd820e2ddc141f8e3ec6bdff48d
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-12-02 00:46:20 -0500

    Update assignment1.bash

--- week13/assignment1.bash
+++ week13/assignment1.bash
@@ -37,6 +37,16 @@ echo ""
 # Example input: JOYC 310
 # Example output: See the screenshots in canvas
 
+function displayLocation(){
+echo -n "Input a course location (ex. FREE, CCM): "
+read courseLocation
+
+echo ""
+echo "Courses in $courseLocation :"
+cat "$courseFile" | grep "$courseLocation" | cut -d';' -f1,2,5,6,7 | sed 's/;/ | /g'
+echo ""
+}
+
 # TODO - 2
 # Make a function that displays all the courses that has availability
 # (seat number will be more than 0) for the given course code
@@ -44,12 +54,29 @@ echo ""
 # Example input: SEC
 # Example output: See the screenshots in canvas
 
+function displayAvailability(){
+
+echo -n "Input course subject (ex. SEC, NET): "
+read courseSubject
+
+echo ""
+echo "Available courses in $courseSubject :"
+grep "^$courseSubject" "$courseFile" | while IFS=";" read -r f1 f2 f3 f4 f5 f6 f7 f8 f9 f10; do
+	if [ "$f4" -gt 0 ]; then
+		echo "$f1 | $f2 | $f3 | $f4 | $f5 | $f6 | $f7 | $f8 | $f9 | $f10"
+	fi
+done
+echo ""
+}
+
 while :
 do
 	echo ""
 	echo "Please select and option:"
 	echo "[1] Display courses of an instructor"
 	echo "[2] Display course count of instructors"
+	echo "[3] Display course(s) in the specified classroom"
+	echo "[4] Display availability of course subjects"
 	echo "[5] Exit"
 
 	read userInput
@@ -65,6 +92,16 @@ do
 	elif [[ "$userInput" == "2" ]]; then
 		courseCountofInsts
 
-	# TODO - 3 Display a message, if an invalid input is given
+	elif [[ "$userInput" == "3" ]]; then
+		displayLocation
+
+	elif [[ "$userInput" == "4" ]]; then
+		displayAvailability
+
+	elif [[ "$userInput" == "5" ]]; then
+		displayAvailability
+		
+	else
+		echo "Come on, how hard can numbers be? Invalid input, try again."
 	fi
 done
