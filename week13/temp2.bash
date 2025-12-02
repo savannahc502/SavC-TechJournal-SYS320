722968043ceecf651cb00091db1bb2f892693c27
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-12-02 01:39:00 -0500

    Update temp2.bash

--- week13/temp2.bash
+++ week13/temp2.bash
@@ -12,9 +12,21 @@ fullPage=$(curl -sL "$link")
 
 # echo "$toolOutput" | paste -d' ' - - - 
 
-echo "$fullPage" | \
-xmlstarlet format --html --recover 2>/dev/null | \
-xmlstarlet select --template \
---match "//html//body//table//tr" \
---value-of "normalize-space(string-join(td,' '))" \
---nl
+# Second Attempt -- string-join only supported in newer XML
+# echo "$fullPage" | \
+# xmlstarlet format --html --recover 2>/dev/null | \
+# xmlstarlet select --template \
+# --match "//html//body//table//tr" \
+# --value-of "normalize-space(string-join(td,' '))" \
+# --nl
+
+temps=$(echo "$fullPage" | \
+xmlstarlet sel -t -m "//table[@id='temp']/tr/td[1]" -v . -n)
+
+times=$(echo "$fullPage" | \
+xmlstarlet sel -t -m "//table[@id='temp']/tr/td[2]" -v . -n)
+
+press=$(echo "$fullPage" | \
+xmlstarlet sel -t -m "//table[@id='press']/tr/td[1]" -v . -n)
+
+paste <(echo "$temps") <(echo "$press") <(echo "$times")
