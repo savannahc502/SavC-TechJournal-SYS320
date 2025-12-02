7c9cf640753eb8a2f6eee48f1f10c7a92d75f680
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-12-02 01:30:33 -0500

    Update temp2.bash

--- week13/temp2.bash
+++ week13/temp2.bash
@@ -3,10 +3,18 @@
 link="http://10.0.17.6/Assignment.html"
 fullPage=$(curl -sL "$link")
 
-toolOutput=$(echo "$fullPage" | \
+# First Attempt
+# toolOutput=$(echo "$fullPage" | \
+# xmlstarlet format --html --recover 2>/dev/null | \
+# xmlstarlet select --template \
+# --value-of "//html//body//table//tr/td" \
+# --nl)
+
+# echo "$toolOutput" | paste -d' ' - - - 
+
+echo "$fullPage" | \
 xmlstarlet format --html --recover 2>/dev/null | \
 xmlstarlet select --template \
---value-of "//html//body//table//tr/td" \
---nl)
-
-echo "$toolOutput" | paste -d' ' - - - 
+--match "//html//body//table//tr" \
+--value-of "normalize-space(string-join(td,' '))" \
+--nl
