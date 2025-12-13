eb70270433faec67e99880031fde9018145836b2
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-12-12 23:15:13 -0500

    Create challenge_1.bash

---
+++ week14/challenge_1.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Author: Savannah Ciak
+# This script scrapes a specific IP address for the text it lists in a table 
+# and saves the output to a text file 
+
+echo ""
+
+curl -s http://10.0.17.IOC.html | \ 
+grep -Eo '(etc/passwd|/bin/bash|/bin/sh|1=1#|1=1--)' > IOC.txt
+cat IOC.txt
